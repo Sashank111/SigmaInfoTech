b61d2990a16f330598287221d7fc097e78434169
sashank adapa <sashankadapa@sashanks-MacBook-Air.local>
2025-11-03 19:58:38 +0530

    Fixed DataTables initialization

--- client/myapp/src/Admin/DeleteOffers.js
+++ client/myapp/src/Admin/DeleteOffers.js
@@ -24,7 +24,7 @@ const DeleteOffers = () => {
             $(function () {
                 new DataTable('#offerTable');
             });
-        }, 200);
+        }, 1000);
     }, []);
 
     const deleteOfferData = (id) => {
